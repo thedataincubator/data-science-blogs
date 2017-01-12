c3a83b65586654ad1830943fa97f30bedd23e7be
Paul Paczuski <pavopax@gmail.com>
2017-01-12 10:12:49 -0800

    ml package list saved with script

---
+++ code/1-get-package-list-from-cran-task-view.py
@@ -0,0 +1,24 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+MAINURL = 'https://cran.r-project.org/web/views/MachineLearning.html'
+
+
+def get_package_list(url):
+  """ scrape URL for list of ML packages, which appears near bottom
+
+  + use regex to remove "(text)" from package name"""
+  r = requests.get(url)
+  soup = BeautifulSoup(r.text, 'lxml')
+  h3 = soup.find('h3')
+  descendants_list = h3.findNextSibling().text.strip().split("\n")
+  return [re.sub(" \([A-z]+\)", "", item) for item in descendants_list]
+
+
+package_list = get_package_list(MAINURL)
+
+print "Found {0} packages.".format(len(package_list))
+
+with open("../package-list-from-cran-task-view.txt", "w") as f:
+  f.write(','.join(package_list))

952492ca66ceae66ab632cfece5d62a5d5b68db2
Paul Paczuski <pavopax@gmail.com>
2017-01-17 22:40:27 -0800

    add scrape of example table

---
+++ code/scrape-computer-world.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+URL = 'http://www.computerworld.com/article/2921176/business-intelligence/great-r-packages-for-data-import-wrangling-visualization.html' # noqa
+
+r = requests.get(URL)
+soup = BeautifulSoup(r.text, 'lxml')
+table = soup.find('table', id='cwsearchabletable')
+pd_table = pd.read_html(str(table))
+if len(pd_table) != 1:
+    print 'WARNING: multiple tables found.  I am using the first one only.'
+df = pd.DataFrame(pd_table[0])
+
+# HERE IS A LIST OF THE R PACKAGES from that link
+print list(df.Package)

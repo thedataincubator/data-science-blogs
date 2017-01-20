e48e84ba30cb881d2c0ff22b1eceef2ea2b68b66
Paul Paczuski <pavopax@gmail.com>
2017-01-19 16:43:13 -0800

    [analysis] update

--- code/analysis.R
+++ code/analysis.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(jsonlite)
 library(forcats)
 library(magrittr)
+library(testthat)
 
 json_dict_to_tbl <- function(file, cols=2, names=c("package", "count")) {
     return(fromJSON(file) %>%
@@ -21,7 +22,12 @@ cran <- fromJSON("../data/cran_metadata.json") %>% tbl_df %>%
     arrange(desc(count)) %>%
     mutate(kind="CRAN",
            rank=dense_rank(desc(count)))
+
+packages <- cran$package %>% u %>% tolower
+
+## filter this one for packages
 so_tags <- fromJSON("../data/so_tags.json") %>% tbl_df %>%
+    filter(name %in% packages) %>% 
     select(name, count) %>%
     rename(package=name) %>%
     mutate(kind="SO tag",
@@ -32,62 +38,52 @@ so_body <- json_dict_to_tbl("../data/body_counts.json") %>%
 so_body_r <- json_dict_to_tbl("../data/body_counts_r.json") %>%
     mutate(kind="SO body + [R]",
            rank=dense_rank(desc(count)))
-
-## GITHUB data
-github0 <- read_csv("../data/github_stars.csv") %>%
-    separate(X1, c("package_author", "package_name"), "/") %>%
-    select(package_name, package_author, starts_with("string")) %>%
-    arrange(package_name, package_author, desc(string))
-
-
-## to long, and select one package_name with highest count
-github <- github0 %>%
-    gather(type, count, starts_with("str")) %>%
-    group_by(package_name) %>%
-    filter(count>0) %>%
-    top_n(1, count) %>%
-    arrange(desc(count), package_name)
-
-github %>% filter(n() > 1)
-
+github <- json_dict_to_tbl("../data/github_stars.json") %>%
+    mutate(kind="github star",
+           rank=dense_rank(desc(count)))
 
 ## ============================================================================
-## data checks
+## check data
 ## ============================================================================
-write.csv(github, "../tmp_github.csv")
-
-github %>% group_by(package_name) %>%
-    filter(n()>1) %>%
-    write.csv("../tmp_dups.csv")
-
+checks <- function(df){
+    namez <- c("package", "count", "kind", "rank")
+    expect_equal(df %>% names %>% length, 4)
+    expect_equal(namez %in% names(df) %>% sum, 4)
+}
+cran %>% checks
+so_tags %>% checks
+github %>% checks
 
 
 ## ============================================================================
 ## analysis
 ## ============================================================================
+df <- bind_rows(cran, so_tags, github) 
 
+top10any <- df %>%
+    filter(rank<11) %>%
+    .$package %>% unique
 
+top10_rank <- df %>%
+    group_by(package) %>%
+    summarise(mean_rank=mean(rank)) %>%
+    top_n(10, desc(mean_rank)) 
 
+top10 <- top10_rank %>% .$package %>% unique
 
-df <- bind_rows(cran, so_tags, so_body, so_body_r) %>%
-    select(-count)
-
-
-top10_packs <- df %>%
-    filter(kind=="SO body + [R]", rank<11) %>%
-    .$package %>% u
 
 df$kind %>% factor %>% levels
+fix <- . %>% factor(levels=c("CRAN", "SO tag", "github star"))
 
-fix <- . %>% factor(levels=c("CRAN", "SO body + [R]", "SO body", "SO tag"))
+dft <- df %>% filter(package %in% top10)
 
 
-
-ggplot(df %>% filter(package %in% top10_packs),
+ggplot(dft,
        aes(x=kind %>% fix, y=rank, group=package, colour=package)) +
     coord_cartesian(ylim = c(1, 20)) +
     scale_y_reverse(breaks = 1:20) +
     scale_x_discrete() +
+    scale_color_brewer(type='qual') +
     geom_line() +
     labs(x="source")
 

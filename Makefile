0b48de726f202b3dd051bf9034449a86813b0dfb
Paul Paczuski <pavopax@gmail.com>
2017-01-27 15:44:38 -0800

    [analysis] add python ranking analysis

--- Makefile
+++ Makefile
@@ -2,29 +2,23 @@ lint:
 	flake8 code/*.py
 
 
-data1:
+r-data:
 	mkdir -p data
 	python code/get_package_list_from_cran_task_view.py
 	python code/get_package_repo_urls.py
-
-data2:
 	python code/get_github_stars.py
 	python code/get_cran_downloads.py
 	python code/get_stack_overflow_data.py
 
-rank-r:
-	echo "Switching to /code..."
-	cd code
-	Rscript analysis.R
+python-data:
+	@echo "See code/make-python-data.ipynb"
 
 help:
-	@echo "    NOTE: Run all scripts from here. EG: `python code/get_cran_metadata.py`"
-	@echo "    "
 	@echo "    lint"
 	@echo "        Check style with flake8."
-	@echo "    data1"
-	@echo "        (Needed for `make data2`)"
-	@echo "        Scrape list of R packages for analysis."
-	@echo "    data2"
-	@echo "        Obtain social metrics using APIs, for a list of packages from data1."
+	@echo "    r-data"
+	@echo "        Scrape list of R packages for analysis and obtain all metrics."
+	@echo "    python-data"
+	@echo "        See code/make-python-data.ipynb"
+
 
--- code/python-analysis.R
+++ code/python-analysis.R
@@ -1,21 +1,125 @@
 library(tidyverse)
 library(magrittr)
 library(stringr)
+library(testthat)
 
-df <- read_csv("../data/python-data.csv")
 
-rank <- df %>%
-    gather(type, count, -package) %>%
+github <- read_csv("../data/python_results_github.csv") %>% arrange(package)
+so <- read_csv("../data/python_results_so.csv") %>% arrange(package)
+pypi <- read_csv("../data/python_results_pypi.csv") %>% arrange(package) 
+
+github$package %<>% tolower
+so$package %<>% tolower
+pypi$package %<>% tolower
+
+
+## FIX DATA
+## github: ipython-notebook is ipython or jupyter/notebook
+## so: jypyter-notebook is converted to ipython-notebook for tags
+## pypi: yes ggplot, not ggpy
+## sqlite3 is base python, so remove
+bad_github <- c("ipython-notebook", "ggplot", "mlpy", "plotly")
+bad_so <- c("design-patterns", "ggplot2", "ggplot", "jupyter-notebook")
+bad_pypi <- c("notebook", "ggpy")
+bad <- c("sqlite3")
+
+github %<>% filter(!(package %in% bad_github))
+so %<>% filter(!(package %in% bad_so))
+pypi %<>% filter(!package %in% bad_pypi)
+
+## SO auto-corrects [basemap] to [matplotlib-basemap] => combine records
+## and [jupyter-notebook] to [ipython-notebook]
+## also, NA tags = 0 tags
+sumx <- . %>% sum(na.rm = TRUE)
+
+so %<>%
+    mutate(package=replace(package,
+                           package=="matplotlib-basemap",
+                           "basemap")) %>%
+    mutate(package=replace(package,
+                           package=="ipython-notebook",
+                           "jupyter-notebook")) %>%
+    group_by(package) %>%
+    summarise(so_tag_counts=sumx(so_tag_counts),
+              so_question_count=sumx(so_question_count))
+
+
+
+
+## CHECKS
+get_unique <- . %>% unique %>% length
+
+## so$package %>% get_unique
+## github$package %>% get_unique
+## pypi$package %>% get_unique
+
+## check dups
+expect_equal(1, so %>% count(package) %>% .$n %>% unique)
+expect_equal(1, github %>% count(package) %>% .$n %>% unique)
+expect_equal(1, pypi %>% count(package) %>% .$n %>% unique)
+expect_false("ggplot" %in% so$package)
+expect_false("ggplot" %in% github$package)
+
+## combine plotly.py and plotly
+## combine ggplot downloads with ggpy results from SO and Github
+full_wide <- so %>%
+    full_join(., github, by="package") %>%
+    full_join(., pypi, by="package") %>%
+    select(-full_name) %>%
+    filter(package != "sqlite3") %>%
+    arrange(package)
+
+plotly_py_forks <- full_wide[full_wide$package=="plotly.py",]$forks
+plotly_py_stars <- full_wide[full_wide$package=="plotly.py",]$stars
+ggplot_downloads <- full_wide[full_wide$package=="ggplot",]$downloads
+
+## FINAL DATA HERE
+full_wide %<>%
+    mutate(forks=replace(forks,
+                         package=="plotly",
+                         plotly_py_forks)) %>%
+    mutate(stars=replace(stars,
+                         package=="plotly",
+                         plotly_py_stars)) %>%
+    mutate(downloads=replace(downloads,
+                             package=="ggpy",
+                             ggplot_downloads)) %>%
+    filter(!(package %in% c("ggplot", "plotly.py")))
+                             
+write_csv(full_wide, "../output/python-data-wide.csv")
+
+
+
+## ============================================================================
+## Analysis
+## ============================================================================
+
+scaled <- full_wide %>%
+    gather(type, value, -package) %>%
+    filter(!is.na(value)) %>%
     group_by(type) %>%
-    mutate(scaled = scale(count) %>% round(., 2)) %>%
-    select(-count) %>%
+    mutate(scaled = scale(value) %>% round(2)) %>%
+    select(-value) %>%
     spread(type, scaled) %>%
+    rename(Package=package, Downloads=downloads)
+
+
+## function to make ranks
+meanx <- . %>% mean(., na.rm=TRUE)
+
+## function to make ranks
+make_ranks <- . %>%
     rowwise() %>%
     mutate(Github=mean(c(forks, stars)) %>% round(2),
            Stack=mean(c(so_question_count, so_tag_counts)) %>% round(2),
-           Overall=mean(c(Github, Stack)) %>% round(2)) %>%
+           Overall=mean(c(Github, Stack, Downloads)) %>% round(2)) %>%
     arrange(desc(Overall)) %>%
-    select(package, Overall, Github, Stack)
+    ungroup() %>%
+    mutate(Rank=dense_rank(desc(Overall))) %>%
+    select(Package, Rank, Overall, Github, Stack, Downloads,
+           forks, stars, starts_with("so"))
 
+## make ranks
+rank_with_na <- scaled %>% make_ranks()
 
-write.csv(rank, "../python-ranks.csv", )
+write_csv(rank_with_na, "../output/python-ranks-with-na.csv")
